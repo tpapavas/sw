78b2a4402e6062ed4ca92cb1b5a36fcc432bf7a5
Nikolaos Giannopoulos <giannopoylos.2@gmail.com>
2026-06-03 15:32:15 +0300

    finalv2

--- kumd/firmware/engine_isr.c
+++ kumd/firmware/engine_isr.c
@@ -175,8 +175,8 @@ int32_t dla_isr_handler(void *engine_data, uint8_t dla_id)
         dla_debug("End CACC 0 counter \n");
         end_cacc_0();
 
-        dla_debug("End CMAC 1 counter \n");
-        end_cmac_1();
+        dla_debug("End CMAC 0 counter \n");
+        end_cmac_0();
 
     }
 
